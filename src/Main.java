54ebb8c0f3d9d69536fa1e8da80fd59ece754ca4
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-08-30 14:15:05 +0700

    commit 2 - исправлены замечания

--- src/Main.java
+++ src/Main.java
@@ -19,19 +19,17 @@ public class Main {
         System.out.println("Задача 2");
         {
             int clientOS = 1;
-            int clientDeviceYear = 2015;
-            if (clientDeviceYear < 2015) {
-                if (clientOS == 0) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-                }
-                if (clientOS == 1) {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-                }
+            int clientDeviceYear = 2013;
+            if (clientDeviceYear < 2015 && clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            }
+            if (clientDeviceYear < 2015 && clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
             } else {
-                if (clientOS == 0) {
+                if (clientDeviceYear >= 2015 && clientOS == 0) {
                     System.out.println("Установите версию приложения для iOS по ссылке.");
                 }
-                if (clientOS == 1) {
+                if (clientDeviceYear >= 2015 && clientOS == 1) {
                     System.out.println("Установите версию приложения для Android по ссылке.");
                 }
             }
@@ -39,42 +37,35 @@ public class Main {
         }
 
         System.out.println("Задача 3");
-        int year = 1995;
+        int year = 2024;
         int everyFourth = year % 4;
         int everyHundredth = year % 100;
         int everyFourthHundredth = year % 400;
-        if (year > 1584) {
-            if (everyFourth == 0 && everyHundredth != 0) {
-                System.out.println(year + " год является високосным.");
-            } else if (everyHundredth == 0 && everyFourthHundredth == 0) {
-                System.out.println(year + " год является високосным.");
-            } else {
-                System.out.println(year + " год является невисокосным.");
-            }
-        } else {
+        if (year > 1584 && everyFourth == 0 && everyHundredth != 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year > 1584 && everyHundredth == 0 && everyFourthHundredth == 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year > 1584 && everyFourthHundredth != 0) {
+            System.out.println(year + " год является невисокосным.");
+        } else if (year < 1584) {
             System.out.println("Извините, високосный год еще не был введен.");
         }
         System.out.println();
 
         System.out.println("Задача 4");
-        int deliveryDistance =95;
-        if (deliveryDistance < 100) {
-            if (deliveryDistance <= 20) {
-                System.out.println("Потребуется дней: 1 сутки доставкию");
-            }
-            else if (deliveryDistance > 20 && deliveryDistance < 60)
-            {
-                System.out.println("Потребуется дней: 2 сутки доставкию");
-            } else {
-                System.out.println("Потребуется дней: 3 сутки доставкию");
-            }
+        int deliveryDistance = 25;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1 сутки на доставку.");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2 суток на доставку.");
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3 суток на доставку.");
         } else System.out.println("Доставки нет!");
         System.out.println();
 
         System.out.println("Задача 5");
-        int monthNumber=11;
-        switch (monthNumber)
-        {
+        int monthNumber = 11;
+        switch (monthNumber) {
             case 12:
             case 1:
             case 2:
